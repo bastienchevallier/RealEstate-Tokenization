42fb7e5c40a82d87dfab849403167dd26f39fc6f
bastienchevallier <bchevallier57@gmail.com>
2019-04-15 17:07:49 -0700

    add ParentContract file, to be completed

---
+++ contracts/ParentContract.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.5.0;
+
+/**
+ * @title ParentContract
+ * @dev This contract defines the main methods to tokenize an asset and create/exchange a performance swap token.
+ */
+
+contract ParentContract {
+} 
